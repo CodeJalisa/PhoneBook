9cb9d5c61fbceaa4d258f61534d481c8682d801d
Jalisa Hewitt <jalisahewitt@Jalisa-ZCW-14.local>
2016-09-28 16:17:50 -0400

    part one

---
+++ src/main/java/Hewitt/Jalisa/PhoneBook.java
@@ -0,0 +1,40 @@
+package Hewitt.Jalisa;
+
+import java.util.HashMap;
+
+/**
+ * Created by jalisahewitt on 9/28/16.
+ */
+public class PhoneBook {
+
+    HashMap<String , String> phoneBook = new HashMap<String, String>();
+
+    public PhoneBook(){}
+
+    public void addContact(String name, String num){
+        phoneBook.put(name,num);
+    }
+
+    public void removeContact(String name, String num){
+        phoneBook.remove(name, num);
+    }
+
+    public String lookUp(String name){
+        String number = phoneBook.get(name);
+        return number;
+    }
+
+    public String allNames(){
+        for(String x : phoneBook.keySet()){
+            return x;
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/Hewitt/Jalisa/PhoneBookTest.java
@@ -0,0 +1,45 @@
+package Hewitt.Jalisa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jalisahewitt on 9/28/16.
+ */
+public class PhoneBookTest {
+
+    @Test
+    public void addContactTest(){
+        PhoneBook testAdd = new PhoneBook();
+        testAdd.addContact("Jalisa", "555555555");
+        String expected = "Jalisa";
+        String actual = testAdd.allNames();
+        assertEquals("Should have added Jalisa to phone book", expected, actual);
+    }
+
+    @Test
+    public void removeContactTest(){
+        PhoneBook testRemove = new PhoneBook();
+        testRemove.addContact("Jalisa", "555555555");
+        testRemove.addContact("Aaron", "302555555");
+        testRemove.removeContact("Jalisa", "555555555");
+        String expected = "Aaron";
+        String actual = testRemove.allNames();
+        assertEquals("Should have removed Jalisa to phone book", expected, actual);
+
+
+    }
+
+    @Test
+    public void lookUp(){
+        PhoneBook testLookUp = new PhoneBook();
+        testLookUp.addContact("Jalisa", "555555555");
+        testLookUp.addContact("Aaron", "302555555");
+        String expected = "302555555";
+        String actual = testLookUp.lookUp("Aaron");
+        assertEquals("Should have showed Aarons number from phone book", expected, actual);
+
+    }
+
+}
